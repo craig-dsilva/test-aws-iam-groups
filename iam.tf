793c18f4f9f1d777f59a2771aff92ba73bc7294a
craig-dsilva <craig.s.dsilva@gmail.com>
2023-12-20 14:17:28 +0000

    IAM users

--- iam.tf
+++ iam.tf
@@ -1,7 +1,15 @@
 resource "aws_iam_user" "devops" {
-  name = "devops"
+  name = "user1"
 }
 
 resource "aws_iam_user" "finance" {
+  name = "user2"
+}
+
+resource "aws_iam_group" "devops" {
+  name = "devops"
+}
+
+resource "aws_iam_group" "finance" {
   name = "finance"
 }
